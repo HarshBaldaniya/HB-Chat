395a8e183ed1d1101b423b3ba89924b55e8fdc31
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-06-22 03:09:15 +0530

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>Hii</Button>;
+};
+
+export default page;
