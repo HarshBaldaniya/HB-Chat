f3939f31098708606b06221d5da7538e12437b6a
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-06-23 01:48:45 +0530

    change some file stucture Check my auth funtion Add add friends small ui part

---
+++ src/components/AddFriendButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { FC } from "react";
+import Button from "./ui/Button";
+
+interface AddFriendButtonProps {}
+
+const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+  return (
+    <form className="max-w-sm">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Add Friend By E-Mail
+      </label>
+
+      <div className="mt-2 flex gap-4">
+        <input
+          type="text"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="you@example.com"
+        />
+        <Button>Add</Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddFriendButton;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,4 +3,6 @@ import { Redis } from "@upstash/redis";
 export const db = new Redis({
   url: 'https://daring-doberman-32696.upstash.io',
   token: 'AX-4ASQgZGRjZTNjMWUtOTQyMC00OTM0LWFmMGMtYzhjY2E4NjNlNTJmNGU4MDhkYjQ2YjkyNDNiNzgxYTQ4Mjg2ODYyMjZiMjI=',
+  // url: process.env.UPSTASH_REDIS_REST_URL,
+  // token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
