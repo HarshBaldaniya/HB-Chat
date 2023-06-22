0da6854f337ef68f45e1a9a9444f9effb09866b8
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-06-22 15:18:58 +0530

    add link

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
-import { Redis } from "@upstash/redis/nodejs";
+import { Redis } from "@upstash/redis";
 
 export const db = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+  url: 'https://daring-doberman-32696.upstash.io',
+  token: 'AX-4ASQgZGRjZTNjMWUtOTQyMC00OTM0LWFmMGMtYzhjY2E4NjNlNTJmNGU4MDhkYjQ2YjkyNDNiNzgxYTQ4Mjg2ODYyMjZiMjI=',
+});
