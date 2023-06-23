1a9e8ad1e3bf5cc1c51946633b50e7214e8dbbe4
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-06-24 00:56:30 +0530

    ADD FRIENDS VALIDATOR Make Add Friend validator System Add Form Library for Check Add Friends Request and validate

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,13 +1,59 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import Button from "./ui/Button";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import axios, { AxiosError } from "axios";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AddFriendButtonProps {}
 
+type FormData = z.infer<typeof addFriendValidator>;
+
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+  const [showSuccessState, setshowSuccessState] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(addFriendValidator),
+  });
+
+  const addFriend = async (email: string) => {
+    try {
+      const validateEmail = addFriendValidator.parse({ email });
+
+      await axios.post("/api/friends/add", {
+        email: validateEmail,
+      });
+
+      setshowSuccessState(true);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        setError("email", { message: error.message });
+        return;
+      }
+
+      if (error instanceof AxiosError) {
+        setError("email", { message: error.response?.data });
+        return;
+      }
+
+      setError("email", { message: "Somthing went wrong." });
+    }
+  };
+
+  const onSubmit = (data: FormData) => {
+    addFriend(data.email);
+  };
+
   return (
-    <form className="max-w-sm">
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm">
       <label
         htmlFor="email"
         className="block text-sm font-medium leading-6 text-gray-900"
@@ -17,12 +63,17 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
       <div className="mt-2 flex gap-4">
         <input
+          {...register("email")}
           type="text"
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="you@example.com"
         />
         <Button>Add</Button>
       </div>
+      <p className="mt-a text-sm text-red-600">{errors.email?.message}</p>
+      {showSuccessState ? (
+        <p className="mt-a text-sm text-green-600">Friend request sent!</p>
+      ) : null}
     </form>
   );
 };
