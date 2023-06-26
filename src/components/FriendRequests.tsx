03f3a20b23d0507c2a74d46d95380498b1df5a61
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-06-26 21:49:51 +0530

    Make full add and remove friend requests

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
 
 interface FriendRequestsProps {
@@ -12,9 +14,31 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   incomingFriendRequests,
   sessionId,
 }) => {
+  const router = useRouter();
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
+
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+
+    router.refresh();
+  };
+
   return (
     <>
       {friendRequests.length === 0 ? (
@@ -25,7 +49,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className="text-black" />
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
-            //   onClick={() => acceptFriend(request.senderId)}
+              onClick={() => acceptFriend(request.senderId)}
               aria-label="accept friend"
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
@@ -33,7 +57,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             </button>
 
             <button
-            //   onClick={() => denyFriend(request.senderId)}
+              onClick={() => denyFriend(request.senderId)}
               aria-label="deny friend"
               className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
